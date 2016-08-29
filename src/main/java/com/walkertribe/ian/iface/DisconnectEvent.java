73a5586631672e366a376a46856d2c379174ba8a
Robert J. Walker <rjw@walkertribe.com>
2016-08-28 20:45:09 -0600

    DisconnectEvent.isNormal()
    The DisconnectEvent can now tell you if the disconnection was "normal"
    or not

--- src/main/java/com/walkertribe/ian/iface/DisconnectEvent.java
+++ src/main/java/com/walkertribe/ian/iface/DisconnectEvent.java
@@ -2,11 +2,9 @@ package com.walkertribe.ian.iface;
 
 /**
  * An event that gets thrown when an existing connection to a remote machine is
- * lost. (A failure to connect in the first place is a
- * {@link ConnectionFailureEvent}.) The {@link #cause} property indicates why
- * the connection was lost. If there is an exception that further explains the
- * event, it is provided in the {@link #exception} property; otherwise, it will
- * be null.
+ * lost. The {@link #cause} property indicates why the connection was lost. If
+ * there is an exception that further explains the event, it is provided in the
+ * {@link #exception} property; otherwise, it will be null.
  * @author rjwut
  */
 public class DisconnectEvent extends ConnectionEvent {
@@ -18,33 +16,39 @@ public class DisconnectEvent extends ConnectionEvent {
 		 * The connection was closed from this side; in other words, the user
 		 * terminated the connection intentionally.
 		 */
-		LOCAL_DISCONNECT,
+		LOCAL_DISCONNECT(true),
 
 		/**
 		 * The connection was closed from the remote side. This could be because
 		 * the remote machine terminated the connection intentionally, or a
 		 * network problem caused the connection to drop.
 		 */
-		REMOTE_DISCONNECT,
+		REMOTE_DISCONNECT(true),
 
 		/**
 		 * IAN received a packet with an unexpected format. This would typically
 		 * be caused by a bug in IAN or in a protocol extension being used by a
 		 * custom proxy.
 		 */
-		PACKET_PARSE_EXCEPTION,
+		PACKET_PARSE_EXCEPTION(false),
 
 		/**
 		 * An I/O exception occurred. The {@link #exception} property may have
 		 * more information, but this is generally an external problem that
 		 * IAN can't do anything about.
 		 */
-		IO_EXCEPTION,
+		IO_EXCEPTION(false),
 
 		/**
 		 * The server version in use is not supported by IAN.
 		 */
-		UNSUPPORTED_SERVER_VERSION
+		UNSUPPORTED_SERVER_VERSION(false);
+
+		private boolean normal;
+
+		private Cause(boolean normal) {
+			this.normal = normal;
+		}
 	}
 
 	private Cause cause;
@@ -64,10 +68,25 @@ public class DisconnectEvent extends ConnectionEvent {
 	}
 
 	/**
+	 * Returns true if this DisconnectEvent is one that would be expected to
+	 * occur when the connection is intentionally terminated. Note that this
+	 * doesn't guarantee that the disconnection was intentional, just that
+	 * it could have been. 
+	 */
+	public boolean isNormal() {
+		return cause.normal;
+	}
+
+	/**
 	 * Returns the Exception that was thrown as a result of the connection
 	 * termination, if any.
 	 */
 	public Exception getException() {
 		return exception;
+	}
+
+	@Override
+	public String toString() {
+		return "Disconnected: " + cause;
 	}
 }
