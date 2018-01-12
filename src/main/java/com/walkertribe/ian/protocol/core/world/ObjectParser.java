625098726524d57ebf464eb191ff2cf0584778db
Robert J. Walker <rjwalker@ldschurch.org>
2018-01-12 15:05:15 -0700

    Added comments

--- src/main/java/com/walkertribe/ian/protocol/core/world/ObjectParser.java
+++ src/main/java/com/walkertribe/ian/protocol/core/world/ObjectParser.java
@@ -4,9 +4,32 @@ import com.walkertribe.ian.iface.PacketReader;
 import com.walkertribe.ian.iface.PacketWriter;
 import com.walkertribe.ian.world.ArtemisObject;
 
+/**
+ * Interface for classes which can parse objects from the payload of an
+ * ObjectUpdatePacket.
+ * @author rjwut
+ */
 public interface ObjectParser {
+	/**
+	 * Returns an array of enum values representing the available properties
+	 * for this type of object. The object will start with a BitField which
+	 * describes which of these properties are present.
+	 */
 	public Enum<?>[] getBits();
+
+	/**
+	 * Reads and returns an object from the payload. If there are no more
+	 * objects in the payload, parse() returns null.
+	 */
 	public ArtemisObject parse(PacketReader reader);
+
+	/**
+	 * Writes the given object to the payload.
+	 */
 	public void write(ArtemisObject obj, PacketWriter writer);
+
+	/**
+	 * Writes the details of the given object to the indicated StringBuilder.
+	 */
 	public void appendDetail(ArtemisObject obj, StringBuilder b);
 }
