6d26d12e1b139ba27842158ce50604b3e8e9bcbb
Robert J. Walker <rjwalker@ldschurch.org>
2018-01-12 15:03:20 -0700

    Exposed ship number, added validation

--- src/main/java/com/walkertribe/ian/protocol/core/setup/SetShipPacket.java
+++ src/main/java/com/walkertribe/ian/protocol/core/setup/SetShipPacket.java
@@ -6,6 +6,7 @@ import com.walkertribe.ian.iface.PacketReader;
 import com.walkertribe.ian.protocol.ArtemisPacket;
 import com.walkertribe.ian.protocol.ArtemisPacketException;
 import com.walkertribe.ian.protocol.core.ValueIntPacket;
+import com.walkertribe.ian.world.Artemis;
 
 /**
  * Set the ship you want to be on. You must send this packet before
@@ -34,10 +35,18 @@ public class SetShipPacket extends ValueIntPacket {
      */
     public SetShipPacket(int shipNumber) {
     	super(SubType.SET_SHIP, shipNumber - 1); // underlying packet wants index
+
+    	if (shipNumber < 1 || shipNumber > Artemis.SHIP_COUNT) {
+    		throw new IllegalArgumentException("Ship number must be between 1 and " + Artemis.SHIP_COUNT);
+    	}
     }
 
     private SetShipPacket(PacketReader reader) {
         super(SubType.SET_SHIP, reader);
+    }
+
+    public int getShipNumber() {
+    	return mArg + 1;
     }
 
     @Override
