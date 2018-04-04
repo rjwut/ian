e9d952e0eea84bd9df34ff77fbdb5775f21625e6
Robert J. Walker <rjw801@gmail.com>
2018-04-04 15:52:48 -0600

    Remove index property for Bases
    We don't know where this property went after v2.3.0.

--- src/main/java/com/walkertribe/ian/world/ArtemisBase.java
+++ src/main/java/com/walkertribe/ian/world/ArtemisBase.java
@@ -8,8 +8,6 @@ import com.walkertribe.ian.enums.ObjectType;
  * Bases
  */
 public class ArtemisBase extends BaseArtemisShielded {
-	private int mIndex = -1;
-
 	public ArtemisBase(int objId) {
         super(objId);
     }
@@ -19,35 +17,13 @@ public class ArtemisBase extends BaseArtemisShielded {
         return ObjectType.BASE;
     }
 
-	/**
-	 * This base's index value. In non-scripted scenarios, DS1's index is 0,
-	 * DS2's index is 1, etc. This value is unique even if the names aren't.
-	 * Unspecified: -1
-	 */
-	public int getIndex() {
-		return mIndex;
-	}
-
-	public void setIndex(int index) {
-		mIndex = index;
-	}
-
     @Override
     public void updateFrom(ArtemisObject obj) {
         super.updateFrom(obj);
-        
-        if (obj instanceof ArtemisBase) {
-            ArtemisBase base = (ArtemisBase) obj;
-
-            if (base.mIndex != -1) {
-            	mIndex = base.mIndex;
-            }
-        }
     }
 
     @Override
 	public void appendObjectProps(SortedMap<String, Object> props) {
     	super.appendObjectProps(props);
-    	putProp(props, "Base index", mIndex, -1);
     }
 }
